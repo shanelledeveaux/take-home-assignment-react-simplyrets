5db4155074fae8575b83b7c9019a6c2365b52d3a
shanellewebster <webstershanelle@gmail.com>
2021-10-17 14:07:46 -0500

    simple unit tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,35 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen, shallow } from '@testing-library/react';
+import Header from 'components/Header/Header';
+import Property from 'components/Property/Property'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const heading = screen.getByRole(
-    'heading',
-    /Side React Take-home Assignment/i,
-  );
-  expect(heading).toBeInTheDocument();
+describe(('Header Tests'), () => {
+  test('header matches snapshot', () => {
+    const component = render(<Header/>);
+
+    expect(component).toMatchSnapshot();
+  });
+
+  test('header contains correct text', () => {
+    render(<Header/>);
+
+    const element = screen.getByText(/Property Listings/i);
+    expect(element).toBeInTheDocument();
+  });
+});
+
+describe('Property Tests', () => {
+  test('property matches snapshot', () => {
+    const component = render(<Property/>);
+  
+    expect(component).toMatchSnapshot();
+  });
+
+  test('property to have listing price', () => {
+    const price = '$4,000,000'
+    render(<Property listPrice={price}/>)
+    
+    expect.objectContaining({
+      price: '$4,000,000'
+    })
+  });
 });
